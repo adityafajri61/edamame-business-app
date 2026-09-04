8cba1aa2d8c8dad581d686e8840668ea166fb9fe
adityafajri61 <157979486+adityafajri61@users.noreply.github.com>
2026-09-04 23:09:52 +0700

    Create apiConfig.js

---
+++ src/config/apiConfig.js
@@ -0,0 +1,9 @@
+/* ==========================================================================
+   KONFIGURASI MODE DATABASE & ENDPOINT GOOGLE APPS SCRIPT
+   ========================================================================== */
+
+// Pilihan MODE: "LOCAL" atau "GOOGLE_SHEETS"
+export const DATA_MODE = "GOOGLE_SHEETS"; 
+
+// Tempelkan Web App URL dari Google Apps Script di sini nanti
+export const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz49gk8Jmw3Bu0RC7k4BcNPr2ZqDnq7GhqB5AF4JoktfgbjM-MWH2YhK2562hGezvYWpQ/exec";
