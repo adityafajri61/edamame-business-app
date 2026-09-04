bceffbdfdf9ca182b9fa16e2d0dbae586188793e
adityafajri61 <157979486+adityafajri61@users.noreply.github.com>
2026-09-05 01:00:04 +0700

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 // Pilihan MODE: "LOCAL" atau "GOOGLE_SHEETS"
-export const DATA_MODE = "GOOGLE_SHEETS"; 
+window.DATA_MODE = "GOOGLE_SHEETS"; 
 
 // Tempelkan Web App URL dari Google Apps Script di sini nanti
-export const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz49gk8Jmw3Bu0RC7k4BcNPr2ZqDnq7GhqB5AF4JoktfgbjM-MWH2YhK2562hGezvYWpQ/exec";
+window.GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz49gk8Jmw3Bu0RC7k4BcNPr2ZqDnq7GhqB5AF4JoktfgbjM-MWH2YhK2562hGezvYWpQ/exec";
